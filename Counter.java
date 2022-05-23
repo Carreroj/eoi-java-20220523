166709b26bb2fe6deaa809b0d0ae81a22c909c5e
Juan <juancantillana84@gmail.com>
2022-05-23 18:03:13 +0200

    Main,java created

--- Counter.java
+++ Counter.java
@@ -2,6 +2,12 @@ public class Counter {
     public static void main(String[] args) {
 
         for (int i = 0;i < 1000;++i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printThousand() {
+        for (int i = 0;i < 1000;++i) {
             if (i%2==0)
                 System.out.println("El número es par");
             else
